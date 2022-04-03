34647e3406de0b75565eb21d3f61665d017f3eab
emilio-tumundo <emilio@tumundo.com>
2022-04-03 16:58:10 -0500

    Final Ui changes for 3rd partial

--- app/src/main/java/com/audifaz/trafficLightStatus/util/Constants.kt
+++ app/src/main/java/com/audifaz/trafficLightStatus/util/Constants.kt
@@ -1,10 +1,8 @@
 package com.audifaz.trafficLightStatus.util
 
-class Constants {
-    const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
-    const val LOCATION_PERMISSION_REQUEST_CODE = 2
-    const val NORDIC_MANUFACTURING_ID = 0x0059
-    const val MANUFACTURING_DATA_SIZE = 17
-    const val UC_NAME = "PS_RD"
-    const val APP_VERSION = "0.0.1"
-}
+const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
+const val LOCATION_PERMISSION_REQUEST_CODE = 2
+const val NORDIC_MANUFACTURING_ID = 0x0059
+const val MANUFACTURING_DATA_SIZE = 17
+const val UC_NAME = "PS_RD"
+const val APP_VERSION = "0.0.1"
